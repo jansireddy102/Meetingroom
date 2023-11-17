46bbd90575afb887431a397a61c674c01802d4da
Giddi Shravya <shravyaevangileena@gmail.com>
2023-11-17 14:51:11 +0530

    Demo

--- EmployeeDemo/src/main/java/com/demo/dao/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.entities.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-	Admin findByEmail(String email);
-}
